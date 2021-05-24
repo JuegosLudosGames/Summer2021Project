06b20e25437b8fb6f48faac23e2be2fa16eb8680
John Amiscaray <john_ernest_amiscaray@outlook.com>
2021-05-24 16:27:09 -0400

    init networkmanager, create playerdetails prefab, added network identity to playerlisting prefab

---
+++ Assets/Scripts/CSharp/Networking/NetworkManagerLobby.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace summer2021.csharp.networking{
+
+    public class NetworkManagerLobby : NetworkManager
+    {
+
+
+
+    }
+
+}
---
+++ Assets/Scripts/CSharp/Networking/PlayerDetails.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+
+namespace summer2021.csharp.networking
+{
+
+    public class PlayerDetails : NetworkBehaviour
+    {
+
+    }
+
+}
+

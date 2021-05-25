70d8d207596f407da3f83fac13d8da085d0e63c5
KyleDulce <36104936+KyleDulce@users.noreply.github.com>
2021-05-25 18:18:41 -0400

    Added Character Scripts

---
+++ Assets/Scripts/CSharp/Game/Character/PlayableCharacter.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace summer2021.csharp.game.character
+{
+    //Added by Kyle | May 25, 2021
+    public class PlayableCharacter : ScriptableObject
+    {
+        [SerializeField] private byte charId;
+        [SerializeField] private Sprite ImageSprite;
+        [SerializeField] private GameObject PlayerPrefab;
+    }
+}
